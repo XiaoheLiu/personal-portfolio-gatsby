114eeb36ad00cc5099634337ee08c6dcba045531
Athena Liu <sdathena@mail.ubc.ca>
2019-04-04 18:32:01 -0700

    Try to resolve case-sensitive naming issue

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Profile from "../components/Profile"
 import Links from "../components/Links"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import SEO from "../components/seo"
 import ProjectList from "../components/ProjectList"
 
